74b7a2a1aa4ece14002fd319af55e1df979ff1bc
André Glatz <andreglatz@outlook.com>
2023-03-07 11:32:04 -0300

    add a shortcut for the match

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ struct CLI {
     path: std::path::PathBuf
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CLI::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line)
         }
     }
+
+    Ok(())
 }
