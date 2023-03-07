1980721f7feac2e8945ece0902d1b6a62bc67a62
André Glatz <andreglatz@outlook.com>
2023-03-07 12:19:59 -0300

    add tests

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,33 @@ fn main() -> Result<()> {
     let path = args.path.to_string_lossy();    
     let content = std::fs::read_to_string(&args.path).with_context(|| format!("could not read file: `{}`", path))?;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line)
-        }
+    let matches = find_matches(&content, &args.pattern);
+    for line in matches {
+        println!("{}", line)
     }
 
     Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str) -> Vec<String> {
+    let mut matches = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(pattern) {
+            matches.push(line.to_string())
+        }
+    }
+
+    return matches;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::find_matches;
+
+    #[test]
+    fn find_a_match() {
+        let result = find_matches("lorem ipsum\ndolor sit amet", "lorem");
+        assert_eq!(result, vec!["lorem ipsum"])
+    }
 }
